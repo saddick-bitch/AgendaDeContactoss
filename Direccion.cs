b563a5250e0d783087bb057ddc86b021bcca6f8f
beatri1304 <168138331+beatri1304@users.noreply.github.com>
2025-03-25 22:13:13 -0600

    Beatriz Robles
    no se que hago aqui

---
+++ Direccion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Class1
+{
+    public Direccion()
+    {
+        public string Calle { get; set; }
+        public string Numero { get; set; }
+        public string Colonia { get; set; }
+        public string Ciudad { get; set; }
+        public string Departamento { get; set; }
+     }
+
+
+
+
+// Constructor para inicializar la dirección
+public Direccion(string calle, string numero, string colonia, string ciudad, string departamento)
+    {
+        Calle = calle;
+        Numero = numero;
+        Colonia = colonia;
+        Ciudad = ciudad;
+        Departamento = departamento;
+
+
+    }
+}
+
+    
+
+
+
+
+
+
